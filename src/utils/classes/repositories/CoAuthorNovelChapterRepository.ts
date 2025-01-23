741091b9efd67f16db5a2928deb3a1a825360780
sakho13 <wecl.khotaro@gmail.com>
2025-01-24 07:50:18 +0900

    ✨ CoAuthorNovelChapterRepositoryとCoAuthorNovelChapterServiceに章の本文を取得・更新するメソッドを追加

--- src/utils/classes/repositories/CoAuthorNovelChapterRepository.ts
+++ src/utils/classes/repositories/CoAuthorNovelChapterRepository.ts
@@ -1,6 +1,12 @@
 import { prisma } from "@/utils/prisma"
 
 export class CoAuthorNovelChapterRepository {
+  /**
+   * 小説本文を除いた情報を取得する
+   * @param userId
+   * @param novelId
+   * @returns
+   */
   public async fetchNovelChapters(userId: string, novelId: string) {
     return await prisma.chapter.findMany({
       select: {
@@ -33,6 +39,51 @@ export class CoAuthorNovelChapterRepository {
             authorId: userId,
           },
         },
+      },
+    })
+  }
+
+  public async fetchChapterContent(
+    userId: string,
+    novelId: string,
+    chapterId: number,
+  ) {
+    return await prisma.chapter.findFirst({
+      select: {
+        content: true,
+      },
+      where: {
+        novel: {
+          authorId: userId,
+        },
+        novelId,
+        id: Number(chapterId),
+      },
+    })
+  }
+
+  public async updateChapterContent(
+    userId: string,
+    novelId: string,
+    chapterId: number,
+    content: string,
+  ) {
+    return await prisma.chapter.update({
+      select: {
+        id: true,
+        novelId: true,
+      },
+      where: {
+        novel: {
+          authorId: userId,
+        },
+        novelId_id: {
+          novelId,
+          id: chapterId,
+        },
+      },
+      data: {
+        content,
       },
     })
   }
--- src/utils/classes/services/CoAuthorNovelChapterService.ts
+++ src/utils/classes/services/CoAuthorNovelChapterService.ts
@@ -8,6 +8,15 @@ export class CoAuthorNovelChapterService {
     private novelChapterRepository: CoAuthorNovelChapterRepository,
   ) {}
 
+  public static create() {
+    const novelRepository = new CoAuthorNovelRepository()
+    const novelChapterRepository = new CoAuthorNovelChapterRepository()
+    return new CoAuthorNovelChapterService(
+      novelRepository,
+      novelChapterRepository,
+    )
+  }
+
   public async fetchNovelChapters(userId: string, novelId: string) {
     const novelNum = await this.novelRepository.countOwnNovels(userId, novelId)
     if (novelNum === 0) {
@@ -39,6 +48,70 @@ export class CoAuthorNovelChapterService {
       userId,
       novelId,
       title,
+    )
+  }
+
+  /**
+   * 小説本文を取得する
+   * @param userId
+   * @param novelId
+   * @param chapterId
+   * @returns
+   */
+  public async fetchChapterContent(
+    userId: string,
+    novelId: string,
+    chapterId: number,
+  ) {
+    const novelNum = await this.novelRepository.countOwnNovels(userId, novelId)
+    if (novelNum === 0) {
+      throw new CoAuthorError({
+        code: "NOVEL_NOT_FOUND",
+        message: "小説が見つかりません。",
+        columns: [],
+      })
+    }
+
+    return await this.novelChapterRepository.fetchChapterContent(
+      userId,
+      novelId,
+      chapterId,
+    )
+  }
+
+  /**
+   * 小説本文を更新する
+   * @param userId
+   * @param novelId
+   * @param chapterId
+   * @param content 本文 (70,000文字以内)
+   * @returns
+   */
+  public async updateChapterContent(
+    userId: string,
+    novelId: string,
+    chapterId: number,
+    content: string,
+  ) {
+    const novelNum = await this.novelRepository.countOwnNovels(userId, novelId)
+    if (novelNum === 0) {
+      throw new CoAuthorError({
+        code: "NOVEL_NOT_FOUND",
+        message: "小説が見つかりません。",
+      })
+    }
+
+    if (content.length > 70000)
+      throw new CoAuthorError({
+        code: "CONTENT_TOO_LONG",
+        message: "本文は70,000文字以内です。",
+      })
+
+    return await this.novelChapterRepository.updateChapterContent(
+      userId,
+      novelId,
+      chapterId,
+      content,
     )
   }
 }
