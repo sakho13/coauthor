c57ffa4f4ec3388a99631be541a0269dbb7a395d
sakho13 <wecl.khotaro@gmail.com>
2025-01-26 01:17:09 +0900

    ✨ GETおよびPOSTメソッドにおけるリクエストパラメータの検証を追加

--- src/app/api/v1/novel/chapter/content/route.ts
+++ src/app/api/v1/novel/chapter/content/route.ts
@@ -1,6 +1,86 @@
 import { CoAuthorApi } from "@/utils/classes/CoAuthorApi"
-import { ApiV1 } from "@/utils/types/CAApiIO"
+import { CoAuthorError } from "@/utils/classes/CoAuthorError"
+import { CoAuthorNovelChapterService } from "@/utils/classes/services/CoAuthorNovelChapterService"
+import { CoAuthorUserService } from "@/utils/classes/services/CoAuthorUserService"
+import {
+  ApiV1,
+  ApiV1ErrorOut,
+  ApiV1ErrorOutColumn,
+} from "@/utils/types/CAApiIO"
 import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(req: NextRequest) {
+  const api = new CoAuthorApi<ApiV1["NovelChapterContent"]["Get"]["Out"]>()
+
+  return await api.executeV2(async () => {
+    const token = await api.verifyAuthorizationHeader(
+      api.parseAuthorizationHeader(req),
+    )
+
+    const novelId = req.nextUrl.searchParams.get("novel_id")
+    const chapterId = req.nextUrl.searchParams.get("chapter_id")
+
+    if (novelId === null) {
+      throw new CoAuthorError({
+        code: "INVALID_PARAMS",
+        message: "リクエスト内容に不整合があります。",
+        columns: [
+          {
+            name: "novel_id",
+            message: "必須パラメータです。",
+          },
+        ],
+      })
+    }
+    if (chapterId === null) {
+      throw new CoAuthorError({
+        code: "INVALID_PARAMS",
+        message: "リクエスト内容に不整合があります。",
+        columns: [
+          {
+            name: "chapter_id",
+            message: "必須パラメータです。",
+          },
+        ],
+      })
+    }
+
+    const parseIntChapterId = parseInt(chapterId)
+    if (isNaN(parseIntChapterId)) {
+      throw new CoAuthorError({
+        code: "INVALID_PARAMS",
+        message: "リクエスト内容に不整合があります。",
+        columns: [
+          {
+            name: "chapter_id",
+            message: "数値である必要があります。",
+          },
+        ],
+      })
+    }
+
+    const userService = CoAuthorUserService.create()
+    const novelChapterService = CoAuthorNovelChapterService.create()
+
+    const user = await userService.fetchUserByFirebaseUid(token.uid)
+
+    return {
+      success: true,
+      data: {
+        novelId: novelId,
+        chapterId: parseIntChapterId,
+        content:
+          (
+            await novelChapterService.fetchChapterContent(
+              user.id,
+              novelId,
+              parseIntChapterId,
+            )
+          )?.content ?? "",
+      },
+    }
+  })
+}
 
 export async function POST(req: NextRequest) {
   const api = new CoAuthorApi<ApiV1["NovelChapterContent"]["Post"]["Out"]>()
@@ -9,16 +89,93 @@ export async function POST(req: NextRequest) {
     const token = await api.verifyAuthorizationHeader(
       api.parseAuthorizationHeader(req),
     )
-    console.log(token)
+
+    const data = await req.json()
+    if (
+      !api.parseValidateResult<ApiV1["NovelChapterContent"]["Post"]["In"]>(
+        data,
+        _validatePost,
+      )
+    )
+      throw new Error("Invalid data")
+
+    const userService = CoAuthorUserService.create()
+    const novelChapterService = CoAuthorNovelChapterService.create()
+
+    const user = await userService.fetchUserByFirebaseUid(token.uid)
+
+    const created = await novelChapterService.updateChapterContent(
+      user.id,
+      data.novelId,
+      data.chapterId,
+      data.content,
+    )
 
     return {
       success: true,
       data: {
-        novelId: "",
-        chapterId: 1,
+        novelId: created.novelId,
+        chapterId: created.id,
       },
     }
   })
 
   return NextResponse.json(data, { status })
+}
+
+function _validatePost(data: unknown): ApiV1ErrorOut | null {
+  const columns: ApiV1ErrorOutColumn[] = []
+
+  if (typeof data !== "object" || data === null)
+    return {
+      code: "INVALID_DATA",
+      message: "入力値が不適切です。",
+      columns: [],
+    }
+
+  if (!("novelId" in data) || typeof data.novelId !== "string")
+    return {
+      code: "INVALID_DATA",
+      message: "入力値が不十分です。",
+      columns: [{ name: "novelId", message: "小説IDは必須です" }],
+    }
+
+  if (!("chapterId" in data) || typeof data.chapterId !== "number")
+    return {
+      code: "INVALID_DATA",
+      message: "入力値が不十分です。",
+      columns: [{ name: "chapterId", message: "章IDは必須です" }],
+    }
+
+  if (!("content" in data) || typeof data.content !== "string")
+    return {
+      code: "INVALID_DATA",
+      message: "入力値が不十分です。",
+      columns: [{ name: "novelType", message: "小説種類は必須です" }],
+    }
+
+  if (data.novelId.length > 0)
+    columns.push({
+      name: "novelId",
+      message: "小説IDは必須です",
+    })
+  if (data.chapterId > 0)
+    columns.push({
+      name: "chapterId",
+      message: "章IDは必須です",
+    })
+  if (data.content.length > 70000)
+    columns.push({
+      name: "content",
+      message: "小説テキストは70000文字以内です",
+    })
+
+  if (columns.length > 0)
+    return {
+      code: "INVALID_DATA",
+      message: "入力値が不適切です。",
+      columns,
+    }
+
+  return null
 }
