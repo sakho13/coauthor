52560ed66eeef2a75e84fa782de00fd37db912e0
sakho13 <wecl.khotaro@gmail.com>
2025-01-28 21:04:02 +0900

    🐛 不要なページタイトルコンポーネントを削除し、CoAuthorEditorを直接表示するように変更

--- src/app/(private)/novel/[novel_id]/c/[order]/page.tsx
+++ src/app/(private)/novel/[novel_id]/c/[order]/page.tsx
@@ -1,4 +1,3 @@
-import { PageTitleParagraph } from "@/components/atoms/PageTitleParagraph"
 import { CoAuthorEditor } from "@/components/organisms/CoAuthorEditor"
 
 type Props = {
@@ -9,15 +8,9 @@ export default async function Page({ params }: Props) {
   const awaitedParams = await params
 
   return (
-    <div className='w-full px-2'>
-      <PageTitleParagraph title='執筆中小説' />
-
-      <div className='mx-16 my-4'>
-        <CoAuthorEditor
-          novelId={awaitedParams.novel_id}
-          order={awaitedParams.order}
-        />
-      </div>
-    </div>
+    <CoAuthorEditor
+      novelId={awaitedParams.novel_id}
+      order={awaitedParams.order}
+    />
   )
 }
