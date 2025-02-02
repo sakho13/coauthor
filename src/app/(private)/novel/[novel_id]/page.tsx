dfdc33fad84d753dd9b99421054bdac5f3640d3a
sakho13 <wecl.khotaro@gmail.com>
2025-02-02 12:11:40 +0900

    ✨ レイアウトを更新し、ページタイトルを追加して小説の表示を改善

--- src/app/(private)/novel/[novel_id]/page.tsx
+++ src/app/(private)/novel/[novel_id]/page.tsx
@@ -1,3 +1,4 @@
+import { PageTitleParagraph } from "@/components/atoms/PageTitleParagraph"
 import { CoAuthorNovelChapterList } from "@/components/organisms/CoAuthorNovelChapterList"
 import { Suspense } from "react"
 
@@ -9,8 +10,14 @@ export default async function Page({ params }: Props) {
   const { novel_id } = await params
 
   return (
-    <Suspense>
-      <CoAuthorNovelChapterList novelId={novel_id} />
-    </Suspense>
+    <div className='w-full h-full px-2'>
+      <PageTitleParagraph title='執筆中小説' />
+
+      <div className='mx-16 my-4 h-full'>
+        <Suspense>
+          <CoAuthorNovelChapterList novelId={novel_id} />
+        </Suspense>
+      </div>
+    </div>
   )
 }
