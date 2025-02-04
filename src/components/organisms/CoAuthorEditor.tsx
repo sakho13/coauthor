23871a8582e74ff4f6668bd8d2cb340fe9b4b1c4
sakho13 <wecl.khotaro@gmail.com>
2025-02-05 07:42:41 +0900

    ✨ CoAuthorEditorコンポーネントのスタイルを追加し、保存機能を改善

--- src/components/organisms/CoAuthorEditor.tsx
+++ src/components/organisms/CoAuthorEditor.tsx
@@ -1,65 +1,84 @@
 "use client"
 
 import "draft-js/dist/Draft.css"
+import "./CoAuthorEditor.css"
 import { CompositeDecorator, ContentState, Editor, EditorState } from "draft-js"
 import { useEffect, useState } from "react"
 import { X } from "lucide-react"
+import { toast } from "sonner"
 import { Button } from "../ui/button"
 import { joinClassName } from "@/utils/functions/joinClassName"
 import { useGetNovelChapterContent } from "@/utils/hooks/useNovelChapterContent"
 import { useApiV1 } from "@/utils/hooks/useApiV1"
 import { useAuthStore } from "@/utils/stores/useAuthStore"
-import { toast } from "sonner"
 
 type Props = { novelId: string; order: number; backRoute: () => void }
 
 export function CoAuthorEditor({ novelId, order, backRoute }: Props) {
-  const { novelTitle, editorState, setEditorState, loading, editorEnable } =
-    useCoAuthorStoryEditor(novelId, order)
+  const {
+    novelTitle,
+    editorState,
+    setEditorState,
+    loading,
+    editorEnable,
+    handleSave,
+  } = useCoAuthorStoryEditor(novelId, order)
 
   return (
     <>
-      <div className='mp-16 border-b'>
-        <Button onClick={backRoute}>
-          <X />
-        </Button>
+      <div id='co-author-editor-header' className='mp-16 border-b flex py-2'>
+        <div className='mx-4'>
+          <Button onClick={backRoute} variant={"secondary"}>
+            <X />
+          </Button>
+        </div>
 
-        <div>
-          <p>{novelTitle}</p>
+        <div className='truncate mx-2 flex items-center'>
+          <p className='text-ellipsis truncate'>{novelTitle}</p>
+        </div>
+
+        <div className='ml-auto mx-4'>
+          <Button onClick={handleSave} disabled={loading}>
+            保存
+          </Button>
         </div>
       </div>
 
-      <div
-        className={joinClassName(
-          "p-2 m-4",
-          "grid grid-cols-2",
-          "h-full",
-          "border",
-        )}
-      >
-        {editorEnable && (
-          <Editor
-            editorKey='CoAuthorEditor'
-            editorState={editorState}
-            onChange={setEditorState}
-            placeholder='執筆を開始する'
-            readOnly={loading}
-          />
-        )}
+      <div id='co-author-editor-main' className='w-full flex flex-col'>
+        <div
+          id='co-author-editor-content'
+          className={joinClassName(
+            "p-2",
+            "grid grid-cols-2",
+            "mt-[32px] mb-[128px] lg:mx-44",
+            "min-h-96 h-full",
+            "border",
+          )}
+        >
+          {editorEnable && (
+            <Editor
+              editorKey='CoAuthorEditor'
+              editorState={editorState}
+              onChange={setEditorState}
+              placeholder='執筆を開始する'
+              readOnly={loading}
+            />
+          )}
+        </div>
       </div>
     </>
   )
 }
 
-type WordCell = {
-  id: string
-  word: string
-  ruby: string | null
-  description: string
-}
+// type WordCell = {
+//   id: string
+//   word: string
+//   ruby: string | null
+//   description: string
+// }
 
 function useCoAuthorStoryEditor(novelId: string, order: number) {
-  const words: WordCell[] = []
+  // const words: WordCell[] = []
 
   const { accessToken } = useAuthStore()
 
@@ -76,50 +95,47 @@ function useCoAuthorStoryEditor(novelId: string, order: number) {
   const [loading, setLoading] = useState(true)
 
   const coAuthorDecorator = new CompositeDecorator([
-    {
-      strategy: (contentBlock, callback, contentState) => {
-        console.log(">> text", contentState)
-        const regex = new RegExp(words.map((word) => word.word).join("|"), "g")
-        const text = contentBlock.getText()
-
-        while (true) {
-          const matched = regex.exec(text)
-          if (matched === null) break
-          const start = matched.index
-          callback(start, start + matched[0].length)
-        }
-      },
-      component: (props) => {
-        if (!props.decoratedText || typeof props.decoratedText !== "string")
-          return <span>{props.children}</span>
-
-        console.log(">> props", props)
-
-        return (
-          <span
-            className='font-bold text-blue-600 underline hover:cursor-pointer'
-            onClick={(event) => {
-              const { target } = event
-              if (target instanceof HTMLElement && target.tagName === "SPAN") {
-                const info = words.find(
-                  (word) => word.word === props.decoratedText,
-                )
-                if (info) _showWordDescription(info)
-              }
-            }}
-            onMouseEnter={() => {}}
-            onMouseLeave={() => {}}
-          >
-            {props.children}
-          </span>
-        )
-      },
-    },
+    // {
+    //   strategy: (contentBlock, callback, contentState) => {
+    //     console.log(">> text", contentState)
+    //     const regex = new RegExp(words.map((word) => word.word).join("|"), "g")
+    //     const text = contentBlock.getText()
+    //     while (true) {
+    //       const matched = regex.exec(text)
+    //       if (matched === null) break
+    //       const start = matched.index
+    //       callback(start, start + matched[0].length)
+    //     }
+    //   },
+    //   component: (props) => {
+    //     if (!props.decoratedText || typeof props.decoratedText !== "string")
+    //       return <span>{props.children}</span>
+    //     console.log(">> props", props)
+    //     return (
+    //       <span
+    //         className='font-bold text-blue-600 underline hover:cursor-pointer'
+    //         onClick={(event) => {
+    //           const { target } = event
+    //           if (target instanceof HTMLElement && target.tagName === "SPAN") {
+    //             const info = words.find(
+    //               (word) => word.word === props.decoratedText,
+    //             )
+    //             if (info) _showWordDescription(info)
+    //           }
+    //         }}
+    //         onMouseEnter={() => {}}
+    //         onMouseLeave={() => {}}
+    //       >
+    //         {props.children}
+    //       </span>
+    //     )
+    //   },
+    // },
   ])
 
-  const _showWordDescription = (word: WordCell) => {
-    console.log(">> word", word)
-  }
+  // const _showWordDescription = (word: WordCell) => {
+  //   console.log(">> word", word)
+  // }
 
   const [editorState, setEditorState] = useState(
     EditorState.createWithContent(
@@ -129,15 +145,18 @@ function useCoAuthorStoryEditor(novelId: string, order: number) {
   )
 
   const _saveContent = async () => {
+    if (loading) return
     if (!dataGetNovelChapterContent || !dataGetNovelChapterContent.success) {
       return
     }
 
-    const currentContent = editorState.getCurrentContent()
-    const plainText = currentContent.getPlainText()
-    console.log(">> plainText", plainText)
+    setLoading(true)
 
     try {
+      const currentContent = editorState.getCurrentContent()
+      const plainText = currentContent.getPlainText()
+      console.log(">> plainText", plainText)
+
       const result = await postChapterContent(accessToken ?? "", {
         novelId: novelId,
         chapterId: dataGetNovelChapterContent.data.chapterId,
@@ -148,6 +167,8 @@ function useCoAuthorStoryEditor(novelId: string, order: number) {
       else toast.error("保存に失敗しました", { description: result.error.code })
     } catch (error) {
       toast.error("保存に失敗しました", { description: JSON.stringify(error) })
+    } finally {
+      setLoading(false)
     }
   }
 
