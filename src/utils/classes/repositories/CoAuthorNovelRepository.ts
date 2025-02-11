ec51842defeeadc17ed85e0220f0e8018c404e69
sakho13 <wecl.khotaro@gmail.com>
2025-02-11 14:17:43 +0900

    ✨ 小説の存在確認機能とデータ更新機能を追加

--- src/utils/classes/repositories/CoAuthorNovelRepository.ts
+++ src/utils/classes/repositories/CoAuthorNovelRepository.ts
@@ -19,6 +19,15 @@ export class CoAuthorNovelRepository {
     })
   }
 
+  public async existsNovel(userId: string, novelId: string) {
+    return !!(await prisma.novel.findFirst({
+      where: {
+        authorId: userId,
+        id: novelId,
+      },
+    }))
+  }
+
   public async countOwnNovels(userId: string, novelId?: string) {
     return await prisma.novel.count({
       where: { authorId: userId, id: novelId },
@@ -37,6 +46,28 @@ export class CoAuthorNovelRepository {
         summary,
         type: Number(novelType),
         status: 0,
+        authorId: userId,
+      },
+    })
+  }
+
+  public async updateNovelData(
+    userId: string,
+    novelId: string,
+    updates: Partial<{
+      title: string
+      summary: string
+      type: keyof typeof Novel_Type
+    }>,
+  ) {
+    return await prisma.novel.update({
+      data: {
+        title: updates.title,
+        summary: updates.summary,
+        type: updates.type,
+      },
+      where: {
+        id: novelId,
         authorId: userId,
       },
     })
